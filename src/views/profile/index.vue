4089be9e0bd97f68ad3022740727a6b3de764807
hejianzhi <hejianzhi@deepexi.com>
2021-11-05 15:58:58 +0800

    feat: 🎸 add profile feature

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,7 +1,51 @@
 <template>
-  <div></div>
+  <div class="my-container">
+    <el-row>
+      <el-col :span="6">
+        <project-card class="user-card" :features="featureData"></project-card>
+      </el-col>
+      <el-col :span="18">
+        <el-card>
+          <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
+              <feature-box :features="featureData" />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
+              <chapter />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.author')" name="author">
+              <author />
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import ProjectCard from './components/ProjectCard'
+import Chapter from './components/Chapter'
+import FeatureBox from './components/FeatureBox'
+import Author from './components/Author'
+import { ref } from 'vue'
+import { feature } from '@/api/user'
+import { watchSwitchLang } from '@/utils/i18n'
+const activeName = ref('feature')
 
-<style lang="scss" scoped></style>
+const featureData = ref([])
+const getFeatureData = async () => {
+  featureData.value = await feature()
+}
+getFeatureData()
+// 监听语言切换
+watchSwitchLang(getFeatureData)
+</script>
+
+<style lang="scss" scoped>
+.my-container {
+  .user-card {
+    margin-right: 20px;
+  }
+}
+</style>
