9c6e583b90a5ea5f2fafd24093560873a925a18c
奕初 <1244653559@qq.com>
2022-04-24 17:40:59 +0800

    feature: init project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
+import router from '@/router';
+// 创建vue实例
+const app = createApp(App)
+
+
+app.use(store) // 挂载pinia
+app.use(router);// 挂载router
+// 挂载实例
+app.mount('#app');
